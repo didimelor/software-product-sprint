7d383b63ff610912d4937ae9b1386f5dc49f8d5c
Diana Melo <a01023785@itesm.mx>
2021-03-05 03:54:06 +0000

    Array that converts to json using Gson

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,18 +1,31 @@
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
+
+//import sun.jvm.hotspot.HelloWorld;
+import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
-@WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet {
+//import sun.jvm.hotspot.HelloWorld;
 
+/** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
+@WebServlet("/first-servlet")
+public class HelloWorldServlet extends HttpServlet {
+   
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+     String[] factsAboutMe = {"My first word as a baby was Tacos", "I love to speak french", "My favorite T.V. show is MasterChef"};
+     String json = convertToJsonUsingGson(factsAboutMe);
+     response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
+  private String convertToJsonUsingGson(String[]factsAboutMe) {
+    Gson gson = new Gson();
+    String json = gson.toJson(factsAboutMe);
+    return json;
   }
 }
